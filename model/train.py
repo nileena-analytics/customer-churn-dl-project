f325a75ff4d6a57d57e66d5d6556abc05d00e2e3
Nileena A N <nileenaanalytics@gmail.com>
2025-10-28 01:39:08 +0000

    initial setup and model

---
+++ model/train.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping
+from tenflow.keras.models import load_model
+
+df  = pd.read_csv('data/customer_churn.csv')
+
+X = df.drop(['CustomerId','Surname','Exited'], axis=1)
+y = df['Exited']
+
+X['Geography'] = LabelEncoder().fit_transform(X['Geography'])
+X['Gender'] = LabelEncoder().fit_transform(X['Gender'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#tranform of the data 
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+
+X_train,X_test,Y_train_Y_test = train_test_split(x,y,test_size=0.2,randome_state=42)
+
+model=Sequential([
+    Dense(16,activation='relu',input_shape=(x_train,shape[1],)),
+    Dense(8,activation='relu')
+    Dense(1,activation='sigmoid')
+])
+
+model.compile(optimizer = 'adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x_train,y_train, epochs= 30, batch_size= 13, validation_split= 0.2, verbose= 1)
+model.save('model/churn_model.h5')
