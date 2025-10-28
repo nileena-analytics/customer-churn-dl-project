c42e7453c39e4d78ca1dc33721338ad70ff9e074
Nileena A N <nileenaanalytics@gmail.com>
2025-10-28 02:12:08 +0000

    model creation

--- models/train.py
+++ models/train.py
@@ -5,9 +5,11 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import EarlyStopping
-from tenflow.keras.models import load_model
+from tensorflow.keras.models import load_model
+import pandas as pd
+import numpy as np
 
-df  = pd.read_csv('data/customer_churn.csv')
+df  = pd.read_csv('data/bank_churn_data.csv')
 
 X = df.drop(['CustomerId','Surname','Exited'], axis=1)
 y = df['Exited']
@@ -20,15 +22,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 
-X_train,X_test,Y_train_Y_test = train_test_split(x,y,test_size=0.2,randome_state=42)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
 model=Sequential([
-    Dense(16,activation='relu',input_shape=(x_train,shape[1],)),
-    Dense(8,activation='relu')
+    Dense(16,activation='relu',input_shape=(X_train.shape[1],)),
+    Dense(8,activation='relu'),
     Dense(1,activation='sigmoid')
 ])
 
 model.compile(optimizer = 'adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(x_train,y_train, epochs= 30, batch_size= 13, validation_split= 0.2, verbose= 1)
+model.fit(X_train,y_train, epochs= 30, batch_size= 13, validation_split= 0.2, verbose= 1)
 model.save('models/churn_model.h5')
-pd.to_pickle(scaler,'.model/scler.pkl')
+pd.to_pickle(scaler,'models/scler.pkl')
